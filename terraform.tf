de09191841344fcb268b7b898bd5d1b35682148e
Sebastián Lorca Delgado <slorcadelgado@gmail.com>
2025-08-06 18:24:07 -0400

    call terraform plan with aws

---
+++ terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "284698404434_slorca-s3-bucket"
+
+  tags = {
+    Owner    = "slorcadelgado"
+    Bootcamp = "devops"
+
+  }
+
+}
